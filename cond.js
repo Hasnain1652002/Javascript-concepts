6c5e8d2f2536c897fee6c8ac693c4f5f0f1d10f6
Hasnain1652002 <hasnainshabbir124@gmail.com>
2024-02-01 23:35:15 +0500

    second commit

---
+++ cond.js
@@ -0,0 +1,9 @@
+console.log(age>18 ? 'Adult' : 'Teen');
+
+let input = prompt('Enter a number')
+
+if (input % 5 === 0){
+    console.log('multiple of 5')
+} else {
+    console.log('not a multiple of 5')
+}
