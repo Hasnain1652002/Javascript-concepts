d4f4aac7391cb753b3223cdea38e9dd067153957
Hasnain <112103324+Hasnain1652002@users.noreply.github.com>
2024-02-06 22:19:29 +0500

    Add files via upload

---
+++ prob3.js
@@ -0,0 +1,8 @@
+let company = ["bloom","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(company);
+company.shift();
+console.log(company);
+company.splice(1,1,"Ola");
+console.log(company);
+company.push("Amozon");
+console.log(company);
